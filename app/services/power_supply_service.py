de69c0d7b2729bfb28b36f18b770e35ee0d0a745
Vladislav <v.v.b.zhuravlev@gmail.com>
2024-12-27 00:48:14 +0300

    pyvisa installed

--- app/services/power_supply_service.py
+++ app/services/power_supply_service.py
@@ -1,3 +1,7 @@
+from pyvisa import ResourceManager
+import logging
+
+
 class PowerSupplyClient:
     """
         :SOURce[1|2|3|4]:CURRent <NRf> 
@@ -14,13 +18,27 @@ class PowerSupplyClient:
 
     """
     def __init__(self):
+        self.rm = ResourceManager('@py')
+
+
+    def init_device(self):
+        connection_address = self.check_connection()
+        self.instrument = self.rm.open_resource(connection_address)
+        healthcheck = self.instrument.query('*IDN?')
+        if healthcheck:
+            logging.info('Устройство подключено и готово к работе')
+
+    def check_connection(self):
+        resources = self.rm.list_resources()
+        if not resources:
+            raise ValueError('Отсутствуют подключения')
+        return resources[0]
+
+    def channel_turn_on():
         pass
 
-    async def channel_turn_on():
+    def channel_turn_off():
         pass
 
-    async def channel_turn_off():
-        pass
-
-    async def channels_state_request():
+    def channels_state_request():
         pass
